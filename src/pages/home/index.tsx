025186b1325182ac27fb7aefac5b68afcb0745d1
peterhvw <jpghvw@gmail.com>
2024-08-30 09:50:47 +0200

    fix(icon import): index.tsx import icons

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -4,6 +4,11 @@ import cla from 'classnames';
 import {FC} from 'react';
 import {useCountState} from '@stores';
 
+import addcircle from '/src/assets/icons/add-circle-outline.svg';
+import chevronforward from '/src/assets/icons/chevron-forward-outline.svg';
+import arrowback from '/src/assets/icons/arrow-back-outline.svg';
+import chevronback from '/src/assets/icons/chevron-back-outline.svg';
+
 const Home: FC = function () {
   const {count, inc, dec} = useCountState();
 
@@ -53,32 +58,51 @@ const Home: FC = function () {
           </button>
         </section>
 
-
         <section>
           <h3>Buttons with Icons</h3>
           <button className={styles.tertiarySmallButton}>
-            Small Icon <img className={styles.iconSmall} src="/src/assets/icons/add-circle-outline.svg" alt="Add" />
+            Small Icon{' '}
+            <img className={styles.iconSmall} src={addcircle} alt="Add" />
           </button>
           <button className={styles.secondaryButton}>
-            Medium Icon <img className={styles.iconMedium} src="/src/assets/icons/chevron-forward-outline.svg" alt="Forward" />
+            Medium Icon{' '}
+            <img
+              className={styles.iconMedium}
+              src={chevronforward}
+              alt="Forward"
+            />
           </button>
           <button className={styles.tertiaryButton}>
-            Large Icon <img className={styles.iconLarge} src="/src/assets/icons/arrow-back-outline.svg" alt="Back" />
+            Large Icon{' '}
+            <img className={styles.iconLarge} src={arrowback} alt="Back" />
           </button>
           <button className={styles.primaryOutlineButton}>
-            Premium Icon <img className={styles.iconPremium} src="/src/assets/icons/add-circle-outline.svg" alt="Add" />
+            Premium Icon{' '}
+            <img className={styles.iconPremium} src={addcircle} alt="Add" />
           </button>
           <button className={styles.secondaryOutlineButton}>
-            Secondary Icon <img className={styles.iconSecondary} src="/src/assets/icons/chevron-back-outline.svg" alt="Back" />
+            Secondary Icon{' '}
+            <img
+              className={styles.iconSecondary}
+              src={chevronback}
+              alt="Back"
+            />
           </button>
           <button className={styles.tertiaryOutlineButton}>
-            Tertiary Icon <img className={styles.iconTertiary} src="/src/assets/icons/chevron-forward-outline.svg" alt="Forward" />
+            Tertiary Icon{' '}
+            <img
+              className={styles.iconTertiary}
+              src={chevronforward}
+              alt="Forward"
+            />
           </button>
           <button className={styles.primaryButton}>
-            Black Icon <img className={styles.iconBlack} src="/src/assets/icons/add-circle-outline.svg" alt="Add" />
+            Black Icon{' '}
+            <img className={styles.iconBlack} src={addcircle} alt="Add" />
           </button>
           <button className={styles.secondaryButton}>
-            White Icon <img className={styles.iconWhite} src="/src/assets/icons/arrow-back-outline.svg" alt="Back" />
+            White Icon{' '}
+            <img className={styles.iconWhite} src={arrowback} alt="Back" />
           </button>
         </section>
 
@@ -87,107 +111,186 @@ const Home: FC = function () {
 
           <div className={styles.buttonShowcase}>
             {/* Input with left icon */}
-            <div className={`${styles.inputWrapper} ${styles.inputWithLeftIcon}`}>
+            <div
+              className={`${styles.inputWrapper} ${styles.inputWithLeftIcon}`}
+            >
               <div className={styles.inputContainer}>
-                <img src="/path/to/left-icon.svg" alt="Left Icon" className={styles.inputIcon} />
-                <input type="text" placeholder="Input with left icon" className={styles.input} />
+                <img src={chevronforward} className={styles.inputIcon} />
+                <input
+                  type="text"
+                  placeholder="Input with left icon"
+                  className={styles.input}
+                />
               </div>
             </div>
 
             {/* Input with right icon */}
-            <div className={`${styles.inputWrapper} ${styles.inputWithRightIcon}`}>
+            <div
+              className={`${styles.inputWrapper} ${styles.inputWithRightIcon}`}
+            >
               <div className={styles.inputContainer}>
-                <input type="text" placeholder="Input with right icon" className={styles.input} />
-                <img src="/path/to/right-icon.svg" alt="Right Icon" className={styles.inputIcon} />
+                <input
+                  type="text"
+                  placeholder="Input with right icon"
+                  className={styles.input}
+                />
+                <img src={chevronforward} className={styles.inputIcon} />
               </div>
             </div>
 
             {/* Input with both icons */}
-            <div className={`${styles.inputWrapper} ${styles.inputWithBothIcons}`}>
+            <div
+              className={`${styles.inputWrapper} ${styles.inputWithBothIcons}`}
+            >
               <div className={styles.inputContainer}>
-                <img src="/path/to/left-icon.svg" alt="Left Icon" className={styles.inputIcon} />
-                <input type="text" placeholder="Input with both icons" className={styles.input} />
-                <img src="/path/to/right-icon.svg" alt="Right Icon" className={styles.inputIcon} />
+                <img src={chevronforward} className={styles.inputIcon} />
+                <input
+                  type="text"
+                  placeholder="Input with both icons"
+                  className={styles.input}
+                />
+                <img src={chevronforward} className={styles.inputIcon} />
               </div>
             </div>
           </div>
-
 
           <div className={styles.buttonShowcase}>
             {/* Default Input */}
             <div className={styles.inputWrapper}>
               <div className={styles.inputContainer}>
-                <input type="text" placeholder="Default Input" className={styles.input} />
+                <input
+                  type="text"
+                  placeholder="Default Input"
+                  className={styles.input}
+                />
               </div>
             </div>
 
             {/* Small Input */}
             <div className={`${styles.inputWrapper} ${styles.inputSmall}`}>
               <div className={styles.inputContainer}>
-                <input type="text" placeholder="Small Input" className={styles.input} />
+                <input
+                  type="text"
+                  placeholder="Small Input"
+                  className={styles.input}
+                />
               </div>
             </div>
 
             {/* Input without Border */}
             <div className={`${styles.inputWrapper} ${styles.inputNoBorder}`}>
               <div className={styles.inputContainer}>
-                <input type="text" placeholder="Input without Border" className={styles.input} />
+                <input
+                  type="text"
+                  placeholder="Input without Border"
+                  className={styles.input}
+                />
               </div>
             </div>
 
             {/* Input with Left Icon */}
-            <div className={`${styles.inputWrapper} ${styles.inputWithLeftIcon}`}>
+            <div
+              className={`${styles.inputWrapper} ${styles.inputWithLeftIcon}`}
+            >
               <div className={styles.inputContainer}>
-                <img src="/src/assets/icons/add-circle-outline.svg" alt="Add" className={styles.inputIcon} />
-                <input type="text" placeholder="Input with Left Icon" className={styles.input} />
+                <img src={addcircle} alt="Add" className={styles.inputIcon} />
+                <input
+                  type="text"
+                  placeholder="Input with Left Icon"
+                  className={styles.input}
+                />
               </div>
             </div>
 
             {/* Input with Right Icon */}
-            <div className={`${styles.inputWrapper} ${styles.inputWithRightIcon}`}>
+            <div
+              className={`${styles.inputWrapper} ${styles.inputWithRightIcon}`}
+            >
               <div className={styles.inputContainer}>
-                <input type="text" placeholder="Input with Right Icon" className={styles.input} />
-                <img src="/src/assets/icons/chevron-forward-outline.svg" alt="Forward" className={styles.inputIcon} />
+                <input
+                  type="text"
+                  placeholder="Input with Right Icon"
+                  className={styles.input}
+                />
+                <img
+                  src={chevronforward}
+                  alt="Forward"
+                  className={styles.inputIcon}
+                />
               </div>
             </div>
 
             {/* Input with Both Icons */}
-            <div className={`${styles.inputWrapper} ${styles.inputWithBothIcons}`}>
+            <div
+              className={`${styles.inputWrapper} ${styles.inputWithBothIcons}`}
+            >
               <div className={styles.inputContainer}>
-                <img src="/src/assets/icons/add-circle-outline.svg" alt="Add" className={styles.inputIcon} />
-                <input type="text" placeholder="Input with Both Icons" className={styles.input} />
-                <img src="/src/assets/icons/chevron-forward-outline.svg" alt="Forward" className={styles.inputIcon} />
+                <img src={addcircle} alt="Add" className={styles.inputIcon} />
+                <input
+                  type="text"
+                  placeholder="Input with Both Icons"
+                  className={styles.input}
+                />
+                <img
+                  src={chevronforward}
+                  alt="Forward"
+                  className={styles.inputIcon}
+                />
               </div>
             </div>
 
             {/* Small Input with Left Icon */}
-            <div className={`${styles.inputWrapper} ${styles.inputSmallWithLeftIcon}`}>
+            <div
+              className={`${styles.inputWrapper} ${styles.inputSmallWithLeftIcon}`}
+            >
               <div className={styles.inputContainer}>
-                <img src="/src/assets/icons/arrow-back-outline.svg" alt="Back" className={styles.inputIcon} />
-                <input type="text" placeholder="Small Input with Left Icon" className={styles.input} />
+                <img src={arrowback} alt="Back" className={styles.inputIcon} />
+                <input
+                  type="text"
+                  placeholder="Small Input with Left Icon"
+                  className={styles.input}
+                />
               </div>
             </div>
 
             {/* Small Input with Right Icon */}
-            <div className={`${styles.inputWrapper} ${styles.inputSmallWithRightIcon}`}>
+            <div
+              className={`${styles.inputWrapper} ${styles.inputSmallWithRightIcon}`}
+            >
               <div className={styles.inputContainer}>
-                <input type="text" placeholder="Small Input with Right Icon" className={styles.input} />
-                <img src="/src/assets/icons/chevron-back-outline.svg" alt="Back" className={styles.inputIcon} />
+                <input
+                  type="text"
+                  placeholder="Small Input with Right Icon"
+                  className={styles.input}
+                />
+                <img
+                  src={chevronback}
+                  alt="Back"
+                  className={styles.inputIcon}
+                />
               </div>
             </div>
 
             {/* Small Input with Both Icons */}
-            <div className={`${styles.inputWrapper} ${styles.inputSmallWithBothIcons}`}>
+            <div
+              className={`${styles.inputWrapper} ${styles.inputSmallWithBothIcons}`}
+            >
               <div className={styles.inputContainer}>
-                <img src="/src/assets/icons/add-circle-outline.svg" alt="Add" className={styles.inputIcon} />
-                <input type="text" placeholder="Small Input with Both Icons" className={styles.input} />
-                <img src="/src/assets/icons/chevron-forward-outline.svg" alt="Forward" className={styles.inputIcon} />
+                <img src={addcircle} alt="Add" className={styles.inputIcon} />
+                <input
+                  type="text"
+                  placeholder="Small Input with Both Icons"
+                  className={styles.input}
+                />
+                <img
+                  src={chevronforward}
+                  alt="Forward"
+                  className={styles.inputIcon}
+                />
               </div>
             </div>
           </div>
         </section>
-
-        
       </div>
     </>
   );
